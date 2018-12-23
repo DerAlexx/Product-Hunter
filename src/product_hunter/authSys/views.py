f0eedc14cab45706a5d1ab7bd3f13d65181a75e7
DerAlexx <alexweb199@hotmail.de>
2018-12-23 21:17:38 +0100

    implemented login

--- src/product_hunter/authSys/views.py
+++ src/product_hunter/authSys/views.py
@@ -7,7 +7,12 @@ def index(request):
 
 def login_(request):
     if request.method == 'POST':
-        return render(request, 'login.html')
+        validusr = auth.authenticate(username=request.POST.get('username'), password=request.POST.get('psw'))
+        if validusr is not None:
+            auth.login(request, validusr)
+            return redirect("/")
+        else:
+            return render(request, 'login.html', {'error':"Check whether your username or password is correct"})  
     else:
         return render(request, 'login.html')
 
@@ -26,20 +31,17 @@ def sign_up_(request):
                             return redirect("/")
                     except User.DoesNotExist as user_ex:
                         print("Will create new User: " + str(request.POST.get('username')))
-                        newuser = User.objects.create_user(username=request.POST.get('username'), password=request.POST.get('psw'))
-                        auth.login(request, newuser)
+                        auth.login(request, User.objects.create_user(username=request.POST.get('username'), password=request.POST.get('psw')))
                         return redirect("/")
                     except Exception as err:
                         print(err)
-                        return render(request, 'signup.html')                       
+                        return render(request, 'signup.html') 
                 else:
                     return render(request, 'signup.html', {'error':"The passwords are not the same"})
             else:
                 return render(request, 'signup.html', {'error':"Password must have atleast 8 Characters"})   
         else:
             return render(request, 'signup.html', {'error':"Username must have at least 3 or more Characters"})
-    elif request.method == 'GET':
-        return render(request, 'signup.html')
     else:
         return render(request, 'signup.html')
     
