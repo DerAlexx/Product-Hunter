d0b2182af10c292b77f2b2727edf4a4fd38c5955
DerAlexx <alexweb199@hotmail.de>
2018-12-24 00:50:01 +0100

    added python-pillow, aswell as the model product also included in the admin panel - installed the apps auth and products - var fixes are also in this commit

--- src/product_hunter/product/models.py
+++ src/product_hunter/product/models.py
@@ -1,1 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class product(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    url = models.TextField()
+    image = models.ImageField(upload_to="../static/static_img/uploads")
+    icon = models.ImageField(upload_to="../static/static_img/uploads_icons")
+    votes = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[0:max]
+
+    def pub_date_awsome(self):
+        return self.pub_date.strftime('%b %e')
+
+    
+
+    
+
+    
