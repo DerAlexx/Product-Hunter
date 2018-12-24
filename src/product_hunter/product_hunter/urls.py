f119887dd39247b9829cbc75e30a1b16af5b9717
DerAlexx <alexweb199@hotmail.de>
2018-12-24 12:16:37 +0100

    var. fixeds and url changes, ready to build the product part of the app

--- src/product_hunter/product_hunter/urls.py
+++ src/product_hunter/product_hunter/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.urls import path,include
 from django.contrib import admin
+from product import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('product.urls')),
+    path('', views.index, name='masterhome'),
+    path('product/', include('product.urls')),
     path('', include('authSys.urls')),
     path('rest/', include('rest_framework.urls', namespace='rest_framework')),
 ]
